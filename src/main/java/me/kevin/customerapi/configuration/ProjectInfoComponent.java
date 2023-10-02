af62cda52c3af51ce8fdafff16121fce0e206b7e
장승호 <sh.jang@fssnuniver.com>
2023-10-03 07:40:57 +0900

    [v0.0.2] Setting up your local MySQL DB and Mybatis

--- src/main/java/me/kevin/customerapi/configuration/ProjectInfoComponent.java
+++ src/main/java/me/kevin/customerapi/configuration/ProjectInfoComponent.java
@@ -14,8 +14,8 @@ import java.util.TimeZone;
 @RequiredArgsConstructor
 public class ProjectInfoComponent {
     public static String NAME = "CUSTOMER-API";
-    public static String VERSION = "v0.0.1";
-    public static String SEQUENCE = "FB3-1";
+    public static String VERSION = "v0.0.2";
+    public static String SEQUENCE = "master";
     public static String DESCRIPTION = "Setting up your local MySQL DB and Mybatis";
 
     private final Environment environment;
