f2bbc7d324cbb571d8c25897bab6e0d2312d2381
장승호 <sh.jang@fssnuniver.com>
2023-10-03 07:56:53 +0900

    [v0.0.4] CRUD APIS

--- src/main/java/me/kevin/customerapi/configuration/ProjectInfoComponent.java
+++ src/main/java/me/kevin/customerapi/configuration/ProjectInfoComponent.java
@@ -14,8 +14,8 @@ import java.util.TimeZone;
 @RequiredArgsConstructor
 public class ProjectInfoComponent {
     public static String NAME = "CUSTOMER-API";
-    public static String VERSION = "v0.0.3";
-    public static String SEQUENCE = "FB5-1";
+    public static String VERSION = "v0.0.4";
+    public static String SEQUENCE = "master";
     public static String DESCRIPTION = "CRUD APIS";
 
     private final Environment environment;
