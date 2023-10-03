1fdad96c6dc44d543d7842f67a03abe8cf6c62a9
장승호 <sh.jang@fssnuniver.com>
2023-10-04 04:35:23 +0900

    [v0.0.5] LOGGING

--- src/main/java/me/kevin/customerapi/configuration/ProjectInfoComponent.java
+++ src/main/java/me/kevin/customerapi/configuration/ProjectInfoComponent.java
@@ -14,8 +14,8 @@ import java.util.TimeZone;
 @RequiredArgsConstructor
 public class ProjectInfoComponent {
     public static String NAME = "CUSTOMER-API";
-    public static String VERSION = "v0.0.4";
-    public static String SEQUENCE = "FB6-1";
+    public static String VERSION = "v0.0.5";
+    public static String SEQUENCE = "master";
     public static String DESCRIPTION = "LOGGING";
 
     private final Environment environment;
